1f89a5fd7db5b49cf3b3987cfa732a373263b0d5
antonia.nistor <antonia.nistor@mail.mcgill.ca>
2022-11-04 19:37:25 -0400

    fix bean service

--- src/services/beanService.ts
+++ src/services/beanService.ts
@@ -2,17 +2,17 @@ import prisma from "../config/prisma";
 import CreateBeanInput from "../interfaces/createBeanInput";
 
 const getById = async (id:number) => {
-    return await prisma.machine.findUnique({ where: { id } });
+    return await prisma.bean.findUnique({ where: { id } });
 };
 
 const create = async (data: CreateBeanInput) => {
-    return await prisma.machine.create({
+    return await prisma.bean.create({
       data,
     });
 };
   
 const deleteById = async (id: number) => {
-    return await prisma.machine.delete({ where: { id } });
+    return await prisma.bean.delete({ where: { id } });
 };
-  
+
 export default { getById, create, deleteById };
