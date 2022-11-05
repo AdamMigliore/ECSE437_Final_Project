1e5e60df54826bb469d0e51a7b791431d69084e9
antonia.nistor <antonia.nistor@mail.mcgill.ca>
2022-11-05 14:07:40 -0400

    create espresso service

---
+++ src/services/espressoService.ts
@@ -0,0 +1,51 @@
+import { Machine } from "@prisma/client";
+import prisma from "../config/prisma";
+import CreateEspressoInput from "../interfaces/createEspressoInput";
+
+const getById = async (id: number) => {
+    return await prisma.espresso.findUnique({ where: { id } });
+};
+
+const create = async (data: CreateEspressoInput) => {
+    const { 
+        beansWeight, 
+        shotTimeInSeconds, 
+        pressure, 
+        notes, 
+        grindSetting, 
+        machineId, 
+        grinderId, 
+        beanId 
+        } = data
+      
+        return await prisma.espresso.create({
+            data: { 
+                beansWeight, 
+                shotTimeInSeconds, 
+                pressure, 
+                notes, 
+                grindSetting,
+                machine: { 
+                    connect: { 
+                        id: machineId
+                    }
+                },
+                grinder: { 
+                    connect: {
+                        id: grinderId
+                    }
+                }, 
+                beans: { 
+                    connect:{
+                        id: beanId
+                    }
+                }
+            }
+        });
+};
+
+const deleteById = async (id: number) => {
+    return await prisma.espresso.delete({ where: { id } });
+};
+
+export default { getById, create, deleteById };
