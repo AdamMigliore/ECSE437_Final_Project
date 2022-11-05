25d936a5d35d93c53a99f306f120c9a11195d1ac
antonia.nistor <antonia.nistor@mail.mcgill.ca>
2022-11-05 14:18:12 -0400

    create controller and router for espresso

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -2,12 +2,14 @@ import express from "express";
 import machineRouter from "./machine";
 import grinderRouter from "./grinder";
 import beanRouter from "./bean";
+import espressoRouter from "./espresso";
 
 const router = express.Router();
 
 router.use("/machine", machineRouter);
 router.use("/grinder", grinderRouter);
 router.use("/bean", beanRouter);
+router.use("/espresso", espressoRouter);
 
 router.get("/", (req, res) => {
   return res.send("hello world");
